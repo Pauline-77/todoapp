741e4e33d8337af9fdaa261348f64dce12961cd7
Pauline-77 <56830289+Pauline-77@users.noreply.github.com>
2019-10-21 16:57:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,49 @@
-coucou
+<?php 
+session_start();
+
+$errors = "";
+
+
+$SESSION = ; 
+$POST= ;  
+$Corbeille = ;
+$Taches= ;
+
+
+?> 
+
+ <!DOCTYPE html>
+<html lang="fr">
+    <meta charset="utf-8">
+    <style type="text/css"> 
+    
+    </style>
+    <head> 
+         <title>to do app</title>
+    </head>
+    <body>
+
+    <form method="POST" action="index.php">
+    <input type="text" name="tache"><br>
+    <button type="submit">Ajouter</button>
+    </form>
+ 
+    <h2>Tâche à faire</h2>
+<table>
+    <tr>
+    	<td> Faire une app</td>
+    	<td> Manger des chips</td>
+    	<td> Cocher des trucs</td>
+    </tr>
+</table>
+
+    <h2>Corbeille</h2>
+
+<table>
+    <tr>
+    	<td>Acheter des chips</td>
+    </tr>
+    
+     </body>
+</html>
+
