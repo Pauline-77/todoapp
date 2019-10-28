207b38e2ffbcc472fe7f0d78a495e1e592e8dde7
Pauline-77 <56830289+Pauline-77@users.noreply.github.com>
2019-10-28 12:23:40 +0100

    preliminaries.php

--- preliminaries.php
+++ preliminaries.php
@@ -29,3 +29,29 @@ $todo= [ empty, $task]
 // Ex7: stocker $todo dans la session (clé "todo")
 
 $SESSION(['todo'])=$todo;
+
+// Ex 8 : écrire une boucle foreach pour faire un var_dump des éléments de $todo
+
+$arr= 
+
+foreach ($arr as $todo){
+
+	 var_drump ($todo)
+    
+}
+
+// Ex 9 : Rajouter d'autres éléments du même type que $task à $todo
+
+
+// Ex 8bis : véridier que la boucle de l'exercice 8 fonctionne toujours
+
+
+// Ex 10 : Afficher, dans des <li> les titres des éléments de todo
+
+
+//Ex 11 : créer une fonction display_task prenant en paramètre une $task et renvoyant le titre dans une balise span, possédant une class "done" si la classe est faite.
+
+
+// Ex 10 bis : remplacer l'affichage du titre par l'appel à la fonction display_task
+
+// https://github.com/smwhr/todoapp/blob/master/preliminaries.php
